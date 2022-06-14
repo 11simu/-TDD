b1e7f4abe4e632736dad8757646c6154cd3663af
jsz <3197278168@qq.com>
2022-06-14 17:29:41 +0800

    重构后

--- functional_tests.py
+++ functional_tests.py
@@ -12,7 +12,14 @@ class NewVisitorTest(unittest.TestCase):   #1
     def tearDown(self):#3
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):#2
+
+    def check_for_row_in_list_table(self, row_text):
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
 
 
         self.browser.get('http://localhost:8000') 
@@ -30,12 +37,14 @@ class NewVisitorTest(unittest.TestCase):   #1
         )
 
 
-        
+        #she types 
         inputbox.send_keys('Buy peacock feathers')
-        
+        #when she hits enter
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')#28+
 
+        #There is still
         inputbox=self.browser.find_element_by_id('id_new_item')#24+
         inputbox.send_keys('Use peacock feathers to make a fly')#
         inputbox.send_keys(Keys.ENTER)#
@@ -43,14 +52,10 @@ class NewVisitorTest(unittest.TestCase):   #1
 
 
 
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
+        #The page updates
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )#24+
 
         self.fail('Finish the test!')
 
