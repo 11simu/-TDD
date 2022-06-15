30063d001994b1c5280f87b6d76faed9cadae3fa
jsz <3197278168@qq.com>
2022-06-15 15:53:42 +0800

    7.52

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from http.client import HTTPResponse
 from django.shortcuts import redirect,render
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def view_list(request):
@@ -12,5 +12,6 @@ def home_page(request):
     return render(request, 'home.html')
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_=List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-word/')
